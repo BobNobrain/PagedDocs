d604530301f327568f23124c4536eeefdd954ff0
Nikita <nikita-ivanov.mail@yandex.ru>
2015-03-15 06:27:19 +0300

    Started ribbon layout styling

---
+++ scripts/ribbon.js
@@ -0,0 +1,24 @@
+
+Ribbon=
+{
+	// Section types
+	SECTION_EMPTY: 0, SECTION_FILLED: 1, SECTION_SEPARATOR: 2, SECTION_VERTICAL_FLOW: 3,
+	
+	create: function(wrapper)
+	{
+		var rbn = { wrapper: wrapper, sections: [] };
+		rbn.createSection=function(name, type)
+		{
+			this.sections.push(Ribbon.createSection(name, type));
+		}
+	},
+	
+	createSection: function(name, type, params)
+	{
+		
+	},
+	
+	createButton: function(label)
+	{
+	}
+}
